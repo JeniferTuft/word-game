deadc3d81cf132537e05849ef07e20f31e290f1f
JeniferTuft <jeniferatuft@gmail.com>
2023-11-02 23:21:21 -0400

    first commit

---
+++ words-db.js
@@ -0,0 +1,58 @@
+let wordList = [
+    {
+        word: "apple",
+        hint: "a teachers favorite fruit"
+    },
+    {
+        word: "banana",
+        hint: "a yellow fruit"
+    },
+    {
+        word: "blueberry",
+        hint: "a blue fruit"
+    },
+    {
+        word: "coconut",
+        hint: "a fruit with delicious milk"
+    },
+    {
+        word: "grape",
+        hint: "a fruit that makes delicious jam"
+    },
+    {
+        word: "kiwi",
+        hint: "a fuzzy fruit"
+    },
+    {
+        word: "lemon",
+        hint: "a sour fruit"
+    },
+    {
+        word: "orange",
+        hint: "a orange fruit"
+    },
+    {
+        word: "peach",
+        hint: "GA state fruit"
+    },
+    {
+        word: "pear",
+        hint: "a green fruit"
+    },
+    {
+        word: "pineapple",
+        hint: "a prickly fruit with sweet flavor"
+    },
+    {
+        word: "strawberry",
+        hint: "a berry with sweet smells"
+    },
+    {
+        word: "tomato",
+        hint: "a fruit used to make delicious sauce"
+    },
+    {
+        word: "watermelon",
+        hint: "a watery fruit"
+    }
+];
